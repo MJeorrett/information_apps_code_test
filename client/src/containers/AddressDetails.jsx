17f6f634f54d7383eb467808281f4d8bc52c3bd9
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-23 16:19:16 +0000

    adding other inputs to Address Details

--- client/src/containers/AddressDetails.jsx
+++ client/src/containers/AddressDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import CountrySelect from '../components/CountrySelect'
+import InputWithIcon from "../components/InputWithIcon"
 
 class AddressDetails extends React.Component {
 
@@ -9,6 +10,10 @@ class AddressDetails extends React.Component {
       <div>
         <h1>Address Details</h1>
         <CountrySelect />
+        <InputWithIcon placeholder="City" />
+        <InputWithIcon placeholder="Post code" />
+        <InputWithIcon placeholder="Address" />
+        <InputWithIcon placeholder="Additional info" />
       </div>
     )
   }
