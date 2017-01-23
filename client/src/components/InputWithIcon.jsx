a78fe3c62dfaeb09f7b35c194251123e8222ec02
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-23 15:31:18 +0000

    adding inputs Personal Details container

---
+++ client/src/components/InputWithIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class InputWithIcon extends React.Component {
+
+  render() {
+    return (
+      <input type="text" placeholder={ this.props.placeholder } />
+    )
+  }
+
+}
+
+export default InputWithIcon
--- client/src/containers/PersonalDetails.jsx
+++ client/src/containers/PersonalDetails.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+import InputWithIcon from '../components/InputWithIcon'
+
 class PersonalDetails extends React.Component {
 
   render() {
     return (
       <div>
         <h1>Personal Details</h1>
+        <InputWithIcon placeholder="First Name"/>
+        <InputWithIcon placeholder="Last Name"/>
+        <InputWithIcon placeholder="E-mail"/>
+        <InputWithIcon placeholder="Phone"/>
       </div>
     )
   }
