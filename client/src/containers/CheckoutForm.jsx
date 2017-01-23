73939078c4fac0e4603a808cbc4d7e3c63fdeef4
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-23 15:21:02 +0000

    adding personal details container

--- client/src/containers/CheckoutForm.jsx
+++ client/src/containers/CheckoutForm.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import PersonalDetails from './PersonalDetails'
+
 class CheckoutForm extends React.Component {
 
   render() {
     return (
       <div>
         <h1>Checkout Form</h1>
+        <PersonalDetails />
       </div>
     )
   }
---
+++ client/src/containers/PersonalDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class PersonalDetails extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Personal Details</h1>
+      </div>
+    )
+  }
+
+}
+
+export default PersonalDetails
